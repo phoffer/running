9ae059cc5d99ca255c3ba19969901f553f4c15fb
Paul Hoffer <paulrhoffer@gmail.com>
2014-12-24 18:30:31 -0700

    add link and shorten delegate for Reading

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,8 +1,8 @@
+# http://api.wunderground.com/api/#{WUNDERGROUND_API_KEY}/history_20141224/q/pws:KAZTUCSO192.json
 class Reading < ActiveRecord::Base
   belongs_to :weather
   belongs_to :station
 
-  delegate :lat, to: :station
-  delegate :lon, to: :station
+  delegate :lat, :lon, to: :station
 
 end
